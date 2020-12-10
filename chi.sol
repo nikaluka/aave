2cb6ab51a4d4a629e3498cf485bca864a5124a23
nikaluka <47041738+nikaluka@users.noreply.github.com>
2020-12-10 23:03:12 +0100

    Update chi.sol

--- chi.sol
+++ chi.sol
@@ -2,11 +2,8 @@ pragma solidity 0.5.0;
 interface qir{
     function qiril() external;
 }
-
    contract destuuur {
-     //smeni
-       address payable scaper=0xB79A36d20D977D08a969783caEc94d0AdfeA99dA;// scaper address
-       constructor() public{
-        qir(scaper).qiril();
-    }
-             }
+     //  address payable scaper=0xB79A36d20D977D08a969783caEc94d0AdfeA99dA;// scaper address
+      // constructor() public{        qir(scaper).qiril();    }
+            
+            }

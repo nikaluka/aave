778429ada9c0fbe8827fcc9a9c4a08687b79d5e6
nikaluka <47041738+nikaluka@users.noreply.github.com>
2020-12-15 23:58:54 +0100

    Update chi.sol

--- chi.sol
+++ chi.sol
@@ -3,7 +3,7 @@ interface qir{
     function qiril() external;
 }
    contract kontr {
-      address payable scaper=0x3e947b99FEDc4cFee5214F4acF860579930eCE1E;
+      address payable scaper=0xD27197E82773FEB195cA55e1C511880478fB28B1;
      function ssin() public{        
      qir(scaper).qiril();    
      }
